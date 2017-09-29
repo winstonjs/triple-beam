4ed928ab85befb12f27509a3e698aad8767c9109
indexzero <charlie.robbins@gmail.com>
2017-09-29 04:36:22 -0400

    [api] Ash nazg durbatulûk, ash nazg gimbatul, ash nazg thrakatulûk, agh burzum-ishi krimpatul

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * A shareable symbol constant that can be used
+ * as a non-enumerable / semi-hidden level identifier
+ * to allow the readable level property to be mutable for
+ * operations like colorization
+ *
+ * @type {Symbol}
+ */
+Object.defineProperty(exports, 'LEVEL', {
+  value: Symbol('level')
+});
